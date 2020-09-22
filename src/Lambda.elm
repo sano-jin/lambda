ddd0a59907138320e0e43c0166d3758601ac729f
Jin SANO <sano-jin@akane.waseda.jp>
2020-09-22 19:23:45 +0900

    updated

--- src/Lambda.elm
+++ src/Lambda.elm
@@ -8,7 +8,6 @@ type alias TermAndFV = { term : TermVal, fv : S.Set String }
 type TermVal = VarVal Int 
              | AppVal TermAndFV TermAndFV
              | LamVal String TermAndFV
-             | Error
 
 getIndexOf : a -> List a -> Result Int Int
 getIndexOf val list =
@@ -45,8 +44,55 @@ lit2TFV termLit env ctx =
             let (bTFV, bCtx) = lit2TFV body (env ++ [var]) ctx in
             ({ term = LamVal var bTFV, fv = S.remove var bTFV.fv}, bCtx)
 
--- show
+shift : TermAndFV -> Int -> Int -> TermAndFV
+shift termAndFV i c =
+    { termAndFV | term = 
+          case termAndFV.term of
+              VarVal n -> if n < c then VarVal n 
+                          else VarVal <| n + i
+              AppVal fun val -> AppVal (shift fun i c)  (shift val i c)
+              LamVal var body -> LamVal var <| shift body i (c + 1)
+    }
 
+substitute : TermAndFV -> TermAndFV -> Int -> TermAndFV
+substitute termAndFV1 termAndFV2 m =
+    case termAndFV1.term of
+        VarVal n -> if n == m then termAndFV2
+                    else termAndFV1
+        AppVal fun val ->
+            let (fun_, val_) =  (substitute fun val m, substitute fun val m) in
+            { term = AppVal fun_ val_
+            , fv = S.union fun_.fv val_.fv
+            }
+        LamVal var body -> let (var_, body_) =
+                                   if S.member var termAndFV2.fv then
+                                       let var__ = newVar var var
+                                                   <| S.union termAndFV2.fv body.fv in
+                                       (var__
+                                       , substitute body {term = VarVal 0
+                                                         , fv = S.singleton var__ } 0)
+                                   else (var, body)
+                               body__ = substitute body_ (shift termAndFV2 1 0) (m + 1)
+                           in { term = LamVal var_ body__
+                              , fv = S.remove var_ body__.fv
+                              }
+
+beta : String -> TermAndFV -> TermAndFV -> TermAndFV
+beta var body val =
+    let val_ = shift val 1 0
+        val__ = substitute body val_ 0
+    in
+        shift val_ -1 0
+                                   
+-- convert to postfix notation
+toPostfixNotation : TermAndFV -> String
+toPostfixNotation tFV =
+    case tFV.term of
+        VarVal i -> String.fromInt i
+        AppVal m n -> toPostfixNotation m ++ toPostfixNotation m ++ "@"
+        LamVal _ body -> toPostfixNotation body ++ "\\"
+
+-- show
 nth : Int -> List a -> Maybe a
 nth i list = case list of
                  [] -> Nothing
@@ -62,14 +108,12 @@ showT tFV env =
                 Nothing -> " Error " -- never reaches here
         AppVal tFV1 tFV2 -> showAppFun tFV1 env ++ showAppVal tFV2 env
         LamVal var body -> "\\" ++ var ++ showCurriedAbs body (var::env) 
-        Error -> " Error "
 
 showCurriedAbs tFV env =
     case tFV.term of
         VarVal _ -> "." ++ showT tFV env
         AppVal tFV1 tFV2 -> "." ++ showAppFun tFV1 env ++ showAppVal tFV2 env
         LamVal var body -> var ++ showCurriedAbs body (var::env) 
-        Error -> " Error "
 
 showAppFun tFV env =
     case tFV.term of
@@ -77,7 +121,6 @@ showAppFun tFV env =
         AppVal tFV1 tFV2 -> showAppFun tFV1 env ++ showAppVal tFV2 env
         LamVal var body ->
             "(\\" ++ var ++ showCurriedAbs body (var::env) ++ ")"
-        Error -> " error "
 
                                     
 showAppVal tFV env =
@@ -85,7 +128,6 @@ showAppVal tFV env =
         VarVal _ -> showT tFV env
         AppVal tFV1 tFV2 -> "(" ++ showAppFun tFV1 env ++ showAppVal tFV2 env ++ ")"
         LamVal var body -> showAppFun tFV env
-        Error -> " error "
                
 newVar : String -> String -> S.Set String -> String
 newVar start var fv =
---
+++ src/VM.elm
@@ -0,0 +1,90 @@
+module VM exposing (..)
+
+import Dict as D
+import Lambda exposing (..)
+import Set as S
+
+type Trans a = BetaTrans (State a)
+             | EtaTrans (State a)
+    
+    
+type State a = State a (List (Trans a))
+             | Join Int
+
+type TermCrumb = AppLeftCrumb TermAndFV (S.Set String)
+               | AppRightCrumb TermAndFV (S.Set String)
+               | LamCrumb String (S.Set String)
+
+type alias TermZipper = (TermAndFV, List TermCrumb)
+
+
+goUpTerm : TermZipper -> TermZipper
+goUpTerm zipper =
+    case zipper of
+        (l, AppLeftCrumb r fv::bs) -> ({ term = AppVal l r, fv = fv }, bs)
+        (r, AppRightCrumb l fv::bs) -> ({ term = AppVal l r, fv = fv }, bs)
+        (body, LamCrumb var fv::bs) -> ({ term = LamVal var body, fv = fv }, bs)
+        top -> top
+    
+topMostTerm : TermZipper -> TermZipper
+topMostTerm zipper =
+    case zipper of
+        (t, []) -> (t, [])
+        z -> topMostTerm (goUpTerm z)
+
+evaluator : TermZipper -> D.Dict String Int -> Int -> (State TermAndFV, D.Dict String Int, Int)
+evaluator (termAndFV, termPath) statesDict i = (Join 0, statesDict, i)
+
+
+evalChildren fun val fv termPath statesDict stateIndex =
+    let (funChildren, funStatesDict, funStateIndex) =
+            eval (fun, AppLeftCrumb val fv::termPath) statesDict stateIndex in
+    let (valChildren, valStatesDict, valStateIndex) =
+            eval (val, AppRightCrumb fun fv::termPath) funStatesDict funStateIndex in
+    (funChildren ++ valChildren, valStatesDict, valStateIndex)
+                
+eval : TermZipper -> D.Dict String Int -> Int -> (List (Trans TermAndFV), D.Dict String Int, Int)
+eval (termAndFV, termPath) statesDict stateIndex =
+        case termAndFV.term of
+            AppVal fun val ->
+                case fun.term of
+                    LamVal var body ->
+                        let (evalued, statesDict_, stateIndex_) = betaTrans var body val termPath statesDict stateIndex in
+                        let (children, statesDict__, stateIndex__) = evalChildren fun val termAndFV.fv termPath statesDict_ stateIndex_ in
+                        (BetaTrans evalued::children, statesDict__, stateIndex__) 
+                    _ -> evalChildren fun val termAndFV.fv termPath statesDict stateIndex
+            LamVal var body ->
+                case body.term of
+                    AppVal fun val ->
+                        if val.term == VarVal 0 && not (S.member var fun.fv) then
+                            let (evalued, statesDict_, stateIndex_) = etaTrans fun termPath statesDict stateIndex in
+                            let (children, statesDict__, stateIndex__) = evalChildren fun val body.fv (LamCrumb var termAndFV.fv::termPath) statesDict_ stateIndex_ in
+                            (EtaTrans evalued::children, statesDict__, stateIndex__) 
+                        else evalChildren fun val body.fv (LamCrumb var termAndFV.fv::termPath) statesDict stateIndex
+                    _ -> eval (body, LamCrumb var termAndFV.fv::termPath) statesDict stateIndex
+            _ -> ([], statesDict, stateIndex)
+
+betaTrans : String -> TermAndFV -> TermAndFV -> List TermCrumb -> D.Dict String Int -> Int -> (State TermAndFV, D.Dict String Int, Int)
+betaTrans var body val termPath statesDict stateIndex =
+    let evaluedTerm = beta var body val
+        (evalued, _) = topMostTerm (evaluedTerm, termPath)
+        postfix = toPostfixNotation evalued
+    in
+        case D.get postfix statesDict of
+            Nothing ->
+                let (children, statesDict_, stateIndex_) =
+                        eval (evalued, []) (D.insert postfix stateIndex statesDict) (stateIndex + 1) in
+                (State evalued children, statesDict_, stateIndex_)
+            Just i -> (Join i, statesDict, stateIndex + 1)
+
+etaTrans : TermAndFV -> List TermCrumb -> D.Dict String Int -> Int -> (State TermAndFV, D.Dict String Int, Int)
+etaTrans fun termPath statesDict stateIndex =
+    let (term, _) = topMostTerm (fun, termPath)
+        postfix = toPostfixNotation term
+    in
+        case D.get postfix statesDict of
+            Nothing ->
+                let (children, statesDict_, stateIndex_) =
+                        eval (term, []) (D.insert postfix stateIndex statesDict) (stateIndex + 1) in
+                (State term children, statesDict_, stateIndex_)
+            Just i -> (Join i, statesDict, stateIndex + 1)
